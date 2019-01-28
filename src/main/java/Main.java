4ed8dd5e6b91d12ce4886b95d1ecc1f0a02c1723
Alina Vlasova <alina13578@yandex.ru>
2019-01-28 18:35:07 +0300

    added documentation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,18 @@
 import Service.Service;
 import java.io.IOException;
 
+/**
+ * Starts XML to JSON converter on http://localhost:8080
+ *
+ * @author Alina Vlasova
+ */
 public class Main {
     private static final int PORT = 8080;
 
+    /**
+     * Method starts service
+     * @param args no args expected
+     */
     public static void main(String[] args) {
         try {
             Service service = new Service(PORT);
--- src/main/java/Service/Service.java
+++ src/main/java/Service/Service.java
@@ -12,6 +12,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.InetSocketAddress;
 
+/**
+ * The class starts http server on specified port, waits and processes POST requests
+ */
 public class Service {
     private static final String PATH = "/json";
     private static final String BAD_REQUEST_MESSAGE = "Bad request";
@@ -21,21 +24,40 @@ public class Service {
 
     private final HttpServer server;
 
+    /**
+     * Method creates http server and handle for processing POST requests
+     * @param port port number
+     * @throws IOException if http server cannot be created
+     */
     public Service(int port) throws IOException {
         this.server = HttpServer.create(new InetSocketAddress(port), 0);
         server.createContext(PATH, createHandler());
     }
 
+    /**
+     * Method starts http server
+     */
     public void start() {
         server.start();
     }
 
+    /**
+     * Method sends response to client
+     * @param http response attributes
+     * @param code response code
+     * @param response content of response
+     * @throws IOException if response cannot be sent
+     */
     private void sendResponse(HttpExchange http, int code, byte[] response) throws IOException {
         http.sendResponseHeaders(code, response.length);
         http.getResponseBody().write(response);
         http.close();
     }
 
+    /**
+     * Method creates handler of incoming POST requests
+     * @return handler
+     */
     private HttpHandler createHandler() {
         return new HttpHandler() {
             @Override
@@ -51,10 +73,23 @@ public class Service {
         };
     }
 
+
+    /**
+     * Method converts xml string to json format
+     * @param xml string in xml format
+     * @return string in json format
+     * @throws JSONException if xml string is incorrect
+     */
     private String convertToJson(String xml) throws JSONException {
         return XML.toJSONObject(xml).toString(4);
     }
 
+    /**
+     * Method saves data from InputStream to string
+     * @param is Input Stream data
+     * @return data string
+     * @throws IOException if data cannot be read
+     */
     private String readInputStreamData(InputStream is) throws IOException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         byte[] data = new byte[1024];
